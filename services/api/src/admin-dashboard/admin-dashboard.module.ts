c0c5ff3878230b2d6d098f610b30df1595428242
霍庆祝 <huoqingzhu@teamcube.cn>
2026-05-10 23:14:56 +0800

    项目审查

--- services/api/src/admin-dashboard/admin-dashboard.module.ts
+++ services/api/src/admin-dashboard/admin-dashboard.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminAuthModule } from '../admin-auth/admin-auth.module';
 import { FeedbackEntity } from '../database/entities/feedback.entity';
 import { OrderEntity } from '../database/entities/order.entity';
 import { ShareRecordEntity } from '../database/entities/share-record.entity';
@@ -9,6 +10,7 @@ import { AdminDashboardService } from './admin-dashboard.service';
 
 @Module({
   imports: [
+    AdminAuthModule,
     TypeOrmModule.forFeature([
       UserEntity,
       OrderEntity,
